fb86ec1522b43f0d7ebd275e0d7ff736ac166c54
= <=>
2022-04-14 15:34:28 -0300

    [Driven][P05]batePapoUOL_0.2_loading_messages_from_API_OK

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //const login = prompt("Login:");
 //const loginInfo = {name: login};
+
+function getMessages(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(loadMessages);
+    promise.catch(codeError);
+}
+
+function loadMessages(response){
+    let pageContent = document.querySelector(".page-content");
+    for(let i = 0; i < response.data.length; i++) {
+        let message = `<div class="message">
+                            <span>${i}</span>
+                            <span class="time">(${response.data[i].time})</span>
+                            <span class="from">${response.data[i].from}</span>
+                            <span class="text">${response.data[i].text}</span>
+                        </div>`
+        pageContent.innerHTML += message;
+    }
+}
+
+function codeError(error){
+    const statusCode = error.response.status;
+    alert(`Error:  ${statusCode}`);
+}
+
+getMessages()
+
