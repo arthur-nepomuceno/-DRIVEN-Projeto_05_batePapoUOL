92e219727eed19125c283a903c31b593de009129
= <=>
2022-04-16 15:29:27 -0300

    [Driven][P05]batePapoUOL_0.4_loading_last_messages_with_scroll_into_view_OK

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 const login = prompt("Login:");
 const loginInfo = {name: login};
 enterChatRoom();
-setInterval(stayOnline, 1 * 3000);
+setInterval(stayOnline, 1 * 4000);
 
 function enterChatRoom(){
     const enterRequest = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", loginInfo);
@@ -30,18 +30,55 @@ function getMessages(){
     promise.catch(codeError);
 }
 
+let lastMessage = "";
+
+
 function loadMessages(response){
     let pageContent = document.querySelector(".page-content");
-    for(let i = 0; i < response.data.length; i++) {
+    for(let i = (response.data.length-30); i < response.data.length; i++) {
         let message = `<div class="message">
-                            <span>${i}</span>
                             <span class="time">(${response.data[i].time})</span>
                             <span class="from">${response.data[i].from}</span>
                             <span class="text">${response.data[i].text}</span>
                         </div>`
         pageContent.innerHTML += message;
+        
     }
+    pageContent.lastChild.scrollIntoView();
+    let lastPosition = response.data.length-1;
+    lastMessage = response.data[lastPosition].text;
 }
+
+
+
+//==================================================================================
+//============================ UPDATING MESSAGES ===============================
+//==================================================================================
+
+function getLastMessage(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(loadLastMessage);
+    promise.catch(codeError);
+}
+
+function loadLastMessage(response){
+    let lastPosition = response.data.length-1;
+
+    if (lastMessage != response.data[lastPosition].text) {
+        let pageContent = document.querySelector(".page-content");
+        let message = `<div class="message">
+                            <span class="time">(${response.data[lastPosition].time})</span>
+                            <span class="from">${response.data[lastPosition].from}</span>
+                            <span class="text">${response.data[lastPosition].text}</span>
+                        </div>`
+        pageContent.innerHTML += message;
+        pageContent.lastChild.scrollIntoView();
+        lastMessage = response.data[lastPosition].text;
+    }
+    return;
+}
+
+setInterval(getLastMessage, 1 * 1000);
 
 
 //==================================================================================
@@ -49,15 +86,17 @@ function loadMessages(response){
 //==================================================================================
 
 function sendMessages() {
-    let inputText = document.querySelector(".bottom input").value;
+    let input = document.querySelector(".bottom input");
     let message = {from: `${login}`, 
     to: "Todos", 
-    text: `${inputText}`,
+    text: `${input.value}`,
     type: "message"}
 
     const request = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", message);
     request.then(loadMessages);
     request.catch(codeError);
+
+    input.value = "";
 }
 
 
